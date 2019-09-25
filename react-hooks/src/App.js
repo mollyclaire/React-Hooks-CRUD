f89a3eb579006483ee8d8bc4c117df30abc6690e
Molly Graham <mollyclaire.graham@gmail.com>
2019-09-25 15:31:08 -0500

    Add user form component added and renders to the DOM.

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import UserTable from './tables/UserTable'
+import AddUserForm from './forms/AddUserForm'
 
 const App = () => {
 
@@ -9,7 +10,16 @@ const App = () => {
     { id: 3, name: 'Ben', username: 'benisphere' },
   ]
 
+  // Hook to render dummy data above to the page
   const [users, setUsers] = useState(usersData)
+
+  // Function to add a new user to the state. It will be passed as a prop in the AddUserForm component.
+  const addUser = user => {
+    // Since this data isn't coming from a database or API, I am manually updating the user.id here
+    user.id = users.length + 1
+    // The spread operator is included to make sure the current users stay in the array
+    setUsers([...users, user])
+  }
 
   return (
     <div className="container">
@@ -17,6 +27,7 @@ const App = () => {
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
+          <AddUserForm addUser={addUser} />
         </div>
         <div className="flex-large">
           <h2>View users</h2>
