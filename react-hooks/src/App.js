100546b2578153555edd7af14a017464d9057b03
Molly Graham <mollyclaire.graham@gmail.com>
2019-09-25 15:12:38 -0500

    Initial files set up with user table component.

---
+++ react-hooks/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import UserTable from './tables/UserTable'
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
